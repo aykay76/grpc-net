280595c9a53a06dc7074d3889fcd51cdce870a52
Alan Kelly <alan.kelly.london@gmail.com>
2020-03-29 14:43:42 +0100

    tidy up

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Threading;
+using Grpc.Core;
 
 namespace grpc_net
 {
     class Program
     {
+        const int Port = 50051;
+
         static void Main(string[] args)
         {
-            
+            Server server = new Server
+            {
+                Services = { GrpcEnvironment.EnvironmentService.BindService(new GrpcEnvironmentServer())},
+                Ports = { new ServerPort("0.0.0.0", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Thread.Sleep(Timeout.Infinite);
+
+            server.ShutdownAsync().Wait();            
         }
     }
 }
