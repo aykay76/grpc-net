e5005be3e408c7e3bec0c19a5c7309f66ddcf920
Alan Kelly <alan.kelly@maersk.com>
2020-03-29 15:58:11 +0100

    example client code

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Grpc.Core;
+using GrpcEnvironment;
 
 namespace grpc_net
 {
@@ -8,8 +10,9 @@ namespace grpc_net
     {
         const int Port = 50051;
 
-        static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
+            // *** example server startup code
             Server server = new Server
             {
                 Services = { GrpcEnvironment.EnvironmentService.BindService(new GrpcEnvironmentServer())},
@@ -17,11 +20,29 @@ namespace grpc_net
             };
             server.Start();
 
+            // *** example client code
+            var channel = new Channel("localhost", 9999, ChannelCredentials.Insecure);
+            var client = new GrpcEnvironment.EnvironmentService.EnvironmentServiceClient(channel);
+            AsyncServerStreamingCall<GrpcEnvironment.KeyValuePair> stream = client.GetEnvironmentVariables(null);
+            while (await stream.ResponseStream.MoveNext()) 
+            {
+                KeyValuePair kvp = stream.ResponseStream.Current;
+                Console.WriteLine($"{kvp.Key} = {kvp.Value}");
+            }
+
+            KeyValuePair request = new KeyValuePair() {
+                Key = "HOSTNAME"
+            };
+            KeyValuePair result = client.GetEnvironmentVariable(request);
+            Console.WriteLine($"{result.Key} = {result.Value}");
+
             Console.WriteLine("Server listening on port " + Port);
             Console.WriteLine("CTRL+C to stop the server...");
             Thread.Sleep(Timeout.Infinite);
 
-            server.ShutdownAsync().Wait();            
+            server.ShutdownAsync().Wait();   
+
+            return 0;         
         }
     }
 }
