80449bb62a013654f2148cfa4d5e47c0e01eff77
Alan Kelly <alan.kelly@maersk.com>
2020-04-02 15:22:23 +0100

    tested and working client and server code

--- Program.cs
+++ Program.cs
@@ -12,35 +12,37 @@ namespace grpc_net
 
         static async Task<int> Main(string[] args)
         {
-            // *** example server startup code
-            Server server = new Server
-            {
-                Services = { GrpcEnvironment.EnvironmentService.BindService(new GrpcEnvironmentServer())},
-                Ports = { new ServerPort("0.0.0.0", Port, ServerCredentials.Insecure) }
-            };
-            server.Start();
+            if (args[0] == "-server") {
+                Server server = new Server
+                {
+                    Services = { GrpcEnvironment.EnvironmentService.BindService(new GrpcEnvironmentServer())},
+                    Ports = { new ServerPort("0.0.0.0", Port, ServerCredentials.Insecure) }
+                };
+                server.Start();
 
-            // *** example client code
-            var channel = new Channel("localhost", 9999, ChannelCredentials.Insecure);
-            var client = new GrpcEnvironment.EnvironmentService.EnvironmentServiceClient(channel);
-            AsyncServerStreamingCall<GrpcEnvironment.KeyValuePair> stream = client.GetEnvironmentVariables(null);
-            while (await stream.ResponseStream.MoveNext()) 
-            {
-                KeyValuePair kvp = stream.ResponseStream.Current;
-                Console.WriteLine($"{kvp.Key} = {kvp.Value}");
+                Console.WriteLine("Server listening on port " + Port);
+                Console.WriteLine("CTRL+C to stop the server...");
+                Thread.Sleep(Timeout.Infinite);
+
+                server.ShutdownAsync().Wait();   
             }
+            else
+            {
+                var channel = new Channel("localhost", Port, ChannelCredentials.Insecure);
+                var client = new GrpcEnvironment.EnvironmentService.EnvironmentServiceClient(channel);
+                AsyncServerStreamingCall<GrpcEnvironment.KeyValuePair> stream = client.GetEnvironmentVariables(new global::Google.Protobuf.WellKnownTypes.Empty());
+                while (await stream.ResponseStream.MoveNext()) 
+                {
+                    KeyValuePair kvp = stream.ResponseStream.Current;
+                    Console.WriteLine($"{kvp.Key} = {kvp.Value}");
+                }
 
-            KeyValuePair request = new KeyValuePair() {
-                Key = "HOSTNAME"
-            };
-            KeyValuePair result = client.GetEnvironmentVariable(request);
-            Console.WriteLine($"{result.Key} = {result.Value}");
-
-            Console.WriteLine("Server listening on port " + Port);
-            Console.WriteLine("CTRL+C to stop the server...");
-            Thread.Sleep(Timeout.Infinite);
-
-            server.ShutdownAsync().Wait();   
+                // KeyValuePair request = new KeyValuePair() {
+                //     Key = "HOSTNAME"
+                // };
+                // KeyValuePair result = client.GetEnvironmentVariable(request);
+                // Console.WriteLine($"{result.Key} = {result.Value}");
+            }
 
             return 0;         
         }
